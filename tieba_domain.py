e7b682eb3f52b4485700a818988edad8bfaf896d
Arrow-Dark <fsjyh1988@163.com>
2018-02-09 18:55:56 +0800

    update

--- tieba_domain.py
+++ tieba_domain.py
@@ -19,7 +19,7 @@ def all_fetcher_thread(rpool, db1,db2,es):
     #     t1.start()
     #     t2.start()
     #     t3.start()
-    for i in range(18):
+    for i in range(3):
         t1=threading.Thread(target=tiezi_fetch.fetch_tiezi,args=(rpool, db1,db2))
         t1.start()
     
